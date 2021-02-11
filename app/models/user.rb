de4a50e91650249526f3a2add188043613515ea3
yamashirory <74095136+yamashirory@users.noreply.github.com>
2021-02-11 13:47:54 +0900

    エラーメッセージまでの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ApplicationRecord
 
 
 validates :nickname, presence: true
-validates :email, presence: true, uniqueness: true, inclusion: { in: "@" }
-validates :encrypted_password, presence: true, length: { minimum: 6 }, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
-validates :encrypted_password_confirmation, presence: true
+validates :email, presence: true, uniqueness: true, inclusion: { in: %w(@) }
+validates :password, presence: true, length: { minimum: 6 }, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
+validates :password_confirmation, presence: true
 validates :name_ue, presence: true, format:{ with: /[ぁ-んァ-ヶ一-龥々]/, message: '全角文字を使用してください' }
 validates :name_shita, presence: true, format:{ with: /[ぁ-んァ-ヶ一-龥々]/, message: '全角文字を使用してください' }
 validates :kana_ue, presence: true, format:{ with:/[ァ-ヶ]/, message: '全角文字を使用してください'}
