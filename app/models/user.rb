4d9d09798bef139040e7e8858a342c7d746fa697
yamashirory <74095136+yamashirory@users.noreply.github.com>
2021-02-12 20:13:59 +0900

    presence: trueの一括

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
-validates :nickname, presence: true
-validates :password, presence: true, length: { minimum: 6 }, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
-validates :password_confirmation, presence: true
-validates :name_ue, presence: true, format:{ with: /[ぁ-んァ-ヶ一-龥々]/ }
-validates :name_shita, presence: true, format:{ with: /[ぁ-んァ-ヶ一-龥々]/ }
-validates :kana_ue, presence: true, format:{ with:/[ァ-ヶ]/ }
-validates :kana_shita, presence: true, format:{ with:/[ァ-ヶ]/ }
-validates :birthday, presence: true
+with_options presence: true do
+validates :nickname
+validates :password, length: { minimum: 6 }, format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]/}
+validates :password_confirmation
+validates :name_ue, format:{ with: /[ぁ-んァ-ヶ一-龥々]/ }
+validates :name_shita, format:{ with: /[ぁ-んァ-ヶ一-龥々]/ }
+validates :kana_ue, format:{ with:/[ァ-ヶ]/ }
+validates :kana_shita, format:{ with:/[ァ-ヶ]/ }
+validates :birthday
+end
 
 end
