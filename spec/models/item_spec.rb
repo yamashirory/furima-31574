9744a018ecf55ae8f9dcdb5dd1834aa701fcb5f4
yamashirory <74095136+yamashirory@users.noreply.github.com>
2021-02-17 17:18:48 +0900

    修正2/17

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
   before do
     @item = FactoryBot.build(:item)
   end
@@ -28,25 +27,50 @@ RSpec.describe Item, type: :model do
       @item.valid?
       expect(@item.errors.full_messages).to include("Category is not a number")
     end
+    it 'カテゴリー情報が1以外でないと登録できない' do
+      @item.category_id = '1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category must be other than 1")
+    end
     it '商品の状態について空では登録ができない' do
       @item.state_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("State is not a number")
+    end
+    it '商品の状態が1以外でないと登録ができない' do
+      @item.state_id = '1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("State must be other than 1")
     end
     it '発送料の負担について空だと登録ができない' do
       @item.shipping_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Shipping is not a number")
     end
+    it '発送料の負担が1以外でないと登録ができない' do
+      @item.shipping_id = '1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping must be other than 1")
+    end
     it '発送元の地域について空だと登録ができない' do
       @item.area_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Area is not a number")
     end
+    it '発送元の地域について1以外出ないと登録ができない' do
+      @item.area_id = '1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Area must be other than 1")
+    end
     it '発送までの日数が空だと登録ができない' do
       @item.day_id = ''
       @item.valid?
       expect(@item.errors.full_messages).to include("Day is not a number")
+    end
+    it '発送までの日数が1以外でないと登録ができない' do
+      @item.day_id = '1'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Day must be other than 1")
     end
     it '価格が空だと登録ができない' do
       @item.price = ''
@@ -65,6 +89,16 @@ RSpec.describe Item, type: :model do
     end
     it '販売価格は半角数字しか登録ができない' do
       @item.price = '３００'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
+    it '販売価格は半角英語だけでは登録できない' do
+      @item.price = 'a'
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Price is not a number")
+    end
+    it '販売価格は半角英数混合では登録できない' do
+      @item.price = 'a1000'
       @item.valid?
       expect(@item.errors.full_messages).to include("Price is not a number")
     end
